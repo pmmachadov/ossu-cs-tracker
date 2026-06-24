410b8508a7a22c600fca66c6e38a49fbb0f28571
Pablo <pmmachadov@gmail.com>
2026-06-24 22:24:10 +0200

    .

--- pruebas/programacion/JAVA IOC/JAVA YOUTUBE/04-Arrays/EjerciciosIniciacion.java
+++ pruebas/programacion/JAVA IOC/JAVA YOUTUBE/04-Arrays/EjerciciosIniciacion.java
@@ -1,9 +1,1 @@
-import java.util.Arrays;
-
-public class EjerciciosIniciacion {
-    public static void main(String[] args) {
-        int[][] array = { { 1, 0, 7 }, { 1, 4, 7 }, { 1, 5, 7 } };
-        // Convierte el array a String y añade varios saltos de línea
-        System.out.println(Arrays.deepToString(array) + "\n\n\n\n\n");
-    }
-}
+// Video_4_10
