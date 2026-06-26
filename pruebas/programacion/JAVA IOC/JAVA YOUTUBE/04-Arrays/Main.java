fb54b644df81fb832aa08591dbf8bf800a49da7a
Pablo <pmmachadov@gmail.com>
2026-06-26 21:11:52 +0200

    .

--- pruebas/programacion/JAVA IOC/JAVA YOUTUBE/04-Arrays/Main.java
+++ pruebas/programacion/JAVA IOC/JAVA YOUTUBE/04-Arrays/Main.java
@@ -1,22 +1,19 @@
-// 🧠 Tu turno (Mini-Reto 8)
-// Vamos a crear el array de los días de la semana (bueno, los 7 días).
-// Escribe el código que haga exactamente esto:
-// Crea un array de String llamado dias con tamaño 7.
-// Guarda "Lunes" en la primera posición.
-// Guarda "Viernes" en la quinta posición.
-// Imprime por pantalla lo que hay en la última posición (el domingo).
-// Pista: Recuerda que los índices empiezan en 0, así que la quinta posición es
-// el índice 4, y la última (séptima) es el índice 6.
-// ¡Tú puedes! Escríbelo y lo revisamos. 💪🏼
+public class Main {
+    public static void main(String[] args) {
+        int[] notas = { 5, 8, 6, 9 };
 
-int[] dia = new int[7];
+        double media = calcularMedia(notas);
 
-dia[0] = "Lunes";
-dia[1] = "Martes";
-dia[2] = "Miercoles";
-dia[3] = "Jueves";
-dia[4] = "Viernes";
-dia[5] = "Sabado";
-dia[6] = "Domingo";
+        System.out.println("Tu nota media es: " + media);
+    }
 
-System.out.println(notas[0]);
+    static double calcularMedia(int[] notas) {
+        int suma = 0;
+
+        for (int n : notas) {
+            suma += n;
+        }
+
+        return suma / (double) notas.length;
+    }
+}
