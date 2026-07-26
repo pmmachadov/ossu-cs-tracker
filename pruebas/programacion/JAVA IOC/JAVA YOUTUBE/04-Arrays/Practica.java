ad6c60800586fe7e2e38fecdd91932ec167f9987
Pablo <pmmachadov@gmail.com>
2026-07-26 13:04:21 +0200

    .

--- pruebas/programacion/JAVA IOC/JAVA YOUTUBE/04-Arrays/Practica.java
+++ pruebas/programacion/JAVA IOC/JAVA YOUTUBE/04-Arrays/Practica.java
@@ -6,18 +6,16 @@ public class Practica {
 
         Scanner sc = new Scanner(System.in);
         int[] numeros = new int[8];
-        System.out.println("Introduce 8 números:");
 
+        mensajeBienvenida();
         for (int i = 0; i < numeros.length; i++) {
-            System.out.print("Nº " + (i + 1) + ": ");
+            pedirNumero(i + 1);
             numeros[i] = sc.nextInt();
         }
 
-        System.out.println("Pares: " + contarPares(numeros));
+        mostrarPares(contarPares(numeros));
         int[] inv = invertir(numeros);
-        System.out.print("Invertido: ");
-        for (int n : inv)
-            System.out.print(n + " ");
+        mostrarInvertido(inv);
         sc.close();
     }
 
@@ -34,5 +32,23 @@ public class Practica {
         for (int i = 0; i < arr.length; i++)
             r[i] = arr[arr.length - 1 - i];
         return r;
+    }
+
+    static void mensajeBienvenida() {
+        System.out.println("Introduce 8 números:");
+    }
+
+    static void pedirNumero(int orden) {
+        System.out.print("Nº " + orden + ": ");
+    }
+
+    static void mostrarPares(int cantidad) {
+        System.out.println("Pares: " + cantidad);
+    }
+
+    static void mostrarInvertido(int[] arr) {
+        System.out.print("Invertido: ");
+        for (int n : arr)
+            System.out.print(n + " ");
     }
 }
