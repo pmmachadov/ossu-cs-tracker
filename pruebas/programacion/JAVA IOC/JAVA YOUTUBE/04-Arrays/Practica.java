e100fa8c0047de562e54db051d5fe7d843d4daeb
Pablo <pmmachadov@gmail.com>
2026-07-26 18:41:36 +0200

    .

--- pruebas/programacion/JAVA IOC/JAVA YOUTUBE/04-Arrays/Practica.java
+++ pruebas/programacion/JAVA IOC/JAVA YOUTUBE/04-Arrays/Practica.java
@@ -1,15 +1,60 @@
 public class Practica {
 
+    static int[] numeros = new int[8];
+
     public static void main(String[] args) {
 
-        // Entrada: Pide 8 números enteros por teclado y los almacena en un array.
+        pideNumeros();
+        int cantPares = contarPares();
+        int[] numInv = invertirNumeros();
+        salidaPares(cantPares);
+        salidaInvertido(numInv);
 
-        // contarPares: Recorre el array y devuelve la cantidad de números pares.
-
-        // invertir: Crea un nuevo array con los elementos en orden inverso al original.
-
-        // Salida: Muestra el número de pares
-
-        // Salida: Array invertido
     }
+    // Entrada: Pide 8 números enteros por teclado y los almacena en un array.
+
+    /*
+     * 
+     * Iniciar scanner
+     * Pedir 8 numeros
+     * recorrer los numeros
+     * agregando N 1,2,...
+     * guardarlos en una variable o array (numeros)
+     * cerrar scanner
+     * 
+     */
+
+    // contarPares: Recorre el array y devuelve la cantidad de números pares.
+
+    /*
+     *
+     * inicializo variable contador
+     * if si es par lo cuento contador++
+     * lo guardo en la variable p
+     * 
+     * return p
+     *
+     * 
+     */
+
+    // invertir: Crea un nuevo array con los elementos en orden inverso al original.
+
+    /*
+     * inicializo el array de numeros y numInv
+     * recorro el array de 0 hasta 7 hacer
+     * array[array.length -1 -i]
+     * guardo el resultado en numInv
+     */
+
+    // Salida: Muestra el número de pares
+
+    /*
+     * Escribir cantPares
+     */
+
+    // Salida: Array invertido
+
+    /*
+     * Escribir snumInv
+     */
 }
