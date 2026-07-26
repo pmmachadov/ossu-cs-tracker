5f9b80be2fd4649cdcf1500a378193bcc468c6f2
Pablo <pmmachadov@gmail.com>
2026-07-26 16:44:39 +0200

    .

--- pruebas/programacion/JAVA IOC/JAVA YOUTUBE/04-Arrays/Practica.java
+++ pruebas/programacion/JAVA IOC/JAVA YOUTUBE/04-Arrays/Practica.java
@@ -1,57 +1,15 @@
-import java.util.Scanner;
-
 public class Practica {
 
-    static int[] numeros = new int[8];
-
     public static void main(String[] args) {
-        pideNumeros();
-        int cantidadPares = contarPares();
-        int[] invertido = invertirNumeros();
-        salidaNumerosPares(cantidadPares);
-        salidaArrayInvertido(invertido);
-    }
 
-    // Entrada: Pide 8 números enteros por teclado y los almacena en un array.
-    public static void pideNumeros() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Introduce 8 numeros enteros:");
-        for (int i = 0; i < numeros.length; i++) {
-            numeros[i] = sc.nextInt();
-        }
-        sc.close();
-    }
+        // Entrada: Pide 8 números enteros por teclado y los almacena en un array.
 
-    // contarPares: Recorre el array y devuelve la cantidad de números pares.
-    public static int contarPares() {
-        int contador = 0;
-        for (int i : numeros) {
-            if (i % 2 == 0) {
-                contador++;
-            }
-        }
-        return contador;
-    }
+        // contarPares: Recorre el array y devuelve la cantidad de números pares.
 
-    // invertir: Crea un nuevo array con los elementos en orden inverso al original.
-    public static int[] invertirNumeros() {
-        int[] num = new int[numeros.length];
-        for (int i = 0; i < num.length; i++) {
-            num[i] = numeros[numeros.length - 1 - i];
-        }
-        return num;
-    }
+        // invertir: Crea un nuevo array con los elementos en orden inverso al original.
 
-    // Salida: Muestra el número de pares y el array invertido.
-    public static void salidaNumerosPares(int contador) {
-        System.out.println("Existen " + contador + " numeros pares.");
-    }
+        // Salida: Muestra el número de pares
 
-    public static void salidaArrayInvertido(int[] num) {
-        System.out.print("Array invertido: ");
-        for (int n : num) {
-            System.out.print(n + " ");
-        }
-        System.out.println();
+        // Salida: Array invertido
     }
 }
--- pruebas/programacion/JAVA IOC/JAVA YOUTUBE/04-Arrays/Practica2.java
+++ pruebas/programacion/JAVA IOC/JAVA YOUTUBE/04-Arrays/Practica2.java
@@ -1,6 +1,66 @@
+import java.util.Scanner;
+
 public class Practica2 {
+
+    static int[] numeros = new int[8];
 
     public static void main(String[] args) {
 
+        pideNumeros();
+        int catidadPares = contarPares();
+        int[] numInvertidos = intertirNumeros();
+        salidaNumerosPares(catidadPares);
+        salidaArrayInvertido(numInvertidos);
     }
+
+    // Entrada: Pide 8 números enteros por teclado y los almacena en un array.
+
+    public static void pideNumeros() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Elige 8 numeros");
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = sc.nextInt();
+        }
+        sc.close();
+    }
+
+    // contarPares: Recorre el array y devuelve la cantidad de números pares.
+
+    public static int contarPares() {
+
+        int contador = 0;
+        for (int i : numeros) {
+            if (i == 0) {
+                contador++;
+            }
+        }
+        return contador;
+    }
+
+    // invertir: Crea un nuevo array con los elementos en orden inverso al original.
+
+    public static int[] intertirNumeros() {
+        int[] n = new int[numeros.length];
+        for (int i = 0; i < n.length; i++) {
+            n[i] = numeros[numeros.length - 1 - i];
+        }
+        return n;
+    }
+
+    // Salida: Muestra el número de pares
+
+    public static void salidaNumerosPares(int contador) {
+        System.out.println("Existen " + contador + " numeros pares.");
+    }
+
+    // Salida: Array invertido
+
+    public static void salidaArrayInvertido(int[] array) {
+        System.out.print("Array invertido: ");
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
 }
