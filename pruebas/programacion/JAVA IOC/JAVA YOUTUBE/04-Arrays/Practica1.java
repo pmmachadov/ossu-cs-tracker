f53f1d41f3948777dfa15f98377710d7cfe3005c
Pablo <pmmachadov@gmail.com>
2026-08-09 20:14:02 +0200

    .

---
+++ pruebas/programacion/JAVA IOC/JAVA YOUTUBE/04-Arrays/Practica1.java
@@ -0,0 +1,18 @@
+public class Practica1 {
+
+    public static void main(String[] args) {
+        int resultado = contarVocales("Hola Mundo, Vamos a contar vocales");
+        System.out.println("Total de vocales: " + resultado);
+    }
+
+    public static int contarVocales(String texto) {
+
+        for (int i = 0; i < texto.length(); i++) {
+            char letra = texto.charAt(i);
+
+        }
+
+        return 0;
+    }
+
+}
